562bdf3dfef5971b51aa8e5fb679f2114f6eec8f
jcobser <70346534+jcobser@users.noreply.github.com>
2022-11-30 16:58:54 +0100

    Add files via upload

---
+++ Servidor Ficheros/CreacionCarpetaParticular.ps1
@@ -0,0 +1,34 @@
+﻿###################################################
+####
+#### Autor: Juan Marcos Cobelo Serantes
+####
+####
+#### Configuración de carpeta particular según
+#### normativa 
+####
+#### 
+####
+#### Copiar el Script a Documentos\Scripts\
+####
+#### Se deben  incluir las herramientas de administración 
+#### del servicio de archivos y recursos compartidos
+####
+###################################################
+
+$SRVFICHEROS = hostname
+$users = Get-ADUser -Filter * -SearchBase (Get-ADDomain).UsersContainer
+foreach ($u in $users){
+    $login = $u.SamAccountName
+    $carpeta =  "\\$SRVFICHEROS\USUARIOS\$login"
+    
+    # Creación y asignación de permisos de la carpeta
+    mkdir $carpeta
+    $acl = Get-Acl $carpeta
+    $acl.SetAccessRuleProtection($True, $False)
+    $grupo = New-Object System.Security.Principal.NTAccount("$login")
+    $acl.AddAccessRule((New-Object System.Security.AccessControl.FileSystemAccessRule($grupo,"FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")))
+    Set-Acl $carpeta $acl
+
+    #Configuración del perfil del usuario
+    Set-ADUser $login -HomeDirectory $carpeta -HomeDrive "P:\"
+}
